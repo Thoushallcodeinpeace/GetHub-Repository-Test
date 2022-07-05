8d4a126e708c330bb18e9f8d7e9cfa42be989497
Thoushallcodeinpeace <108714580+Thoushallcodeinpeace@users.noreply.github.com>
2022-07-05 14:13:11 +0800

    Initial commit

---
+++ HelloWorld3.java
@@ -0,0 +1,7 @@
+public class HelloWorld3
+{
+	public static void main (String[] args)
+	{
+		System.out.println("\n Mag start naak F2F, ayaw na pag dugay dugay ka kabayaan ka sa imong mga amigo ug miga ");
+	}
+}
