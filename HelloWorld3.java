e9f34ef00827f9ee88b8d196c1c756478f9c0718
Thoushallcodeinpeace <108714580+Thoushallcodeinpeace@users.noreply.github.com>
2022-07-05 14:42:43 +0800

    Update HelloWorld3.java

--- HelloWorld3.java
+++ HelloWorld3.java
@@ -2,6 +2,6 @@ public class HelloWorld3
 {
 	public static void main (String[] args)
 	{
-		System.out.println("\n Good luck with the face to face classes ");
+		System.out.println("\n Good luck with the face to face classes (Test Branch) ");
 	}
 }
